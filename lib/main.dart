7c41caef8c4c65894e8f6f2ae7dfc22a06119a36
BrushankPatra <72485994+BrushankPatra@users.noreply.github.com>
2023-03-11 12:58:48 +0530

    questions added

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,19 @@ void main() => runApp(UnoriginalApp());
 class UnoriginalApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    const questions = [
+      "Which car manufacturer do you like?",
+      "Which smartphone brand do you prefer?",
+      "Which brand do you like for its laptops?",
+      "Which tech YouTube channel do you like the most?",
+      "For which of the following services do you pay a premium?"
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text("Quiz App"),
         ),
-        body: Text("This is the default text!"),
+        body: Column(children: []),
       ),
     );
   }
